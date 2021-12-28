b73de7d37a8b2a9385daf3d0d0d82067fa6f0639
Mohammad Dicky Andika Putra <mohammaddicky.andika@gmail.com>
2021-12-28 18:26:42 +0700

    get more violators using card posts

--- cogs/detect_posts.py
+++ cogs/detect_posts.py
@@ -67,47 +67,53 @@ class detect_posts(commands.Cog, name="detect posts"):
         await ctx.reply(reply)
 
     @commands.command()
-    async def card(self, ctx, arg1):
-        page_source = self.get_page_sources('https://apps.qoo-app.com/en/app-card/' + str(arg1))
-        
-        posts_list = page_source.findAll("div", class_="qoo-post-item")
-        
-        post_info_list = []
-        for post in posts_list:
-            post_info = {"user": "", "link": "", "char_name": "", "guild_name": "", "desc": ""}
-        
-            post_info["user"] = post.select('a.icon.qoo-egg-view')[0]["href"]
-            post_info["link"] = 'https://user.qoo-app.com/en/card/' + post["data-id"]
-            td = post.findAll('td')
-            if len(td)>0 :
-                post_info["char_name"] = self.normalize(td[1].get_text(strip=True))
-            if len(td)>2 :
-                post_info["guild_name"] = self.normalize(td[3].get_text(strip=True))
+    async def card(self, ctx, arg1 = ""):
 
-            desc = post.findAll('p')
-            if len(desc) > 1:
-                post_info["desc"] = self.normalize(desc[1].get_text(strip=True))
-
-            post_info_list.append(post_info)
-           
-        with open("./cogs/blackwordlist.json", 'r', encoding='utf-8-sig') as file:
-            word_list = json.load(file)["word_list"]
-        
-        violating_post = []
-        for post in post_info_list:
-            if any(word in post["char_name"] for word in word_list) or any(word in post["desc"] for word in word_list) or any(word in post["guild_name"] for word in word_list) :
-                violating_post.append(post)
-        
-        reply = ""
-        for post in violating_post:
-            reply = reply + post["user"] + '\n'
-            reply = reply + post["link"] + '\n'
+        if arg1 != "":
+            page_source = self.get_page_sources('https://apps.qoo-app.com/en/app-card/' + str(arg1))
             
-        if reply == "":
-            reply = "No violators detected."
-        reply = reply + '\n'
-        
-        await ctx.reply(reply)
+            posts_list = page_source.findAll("div", class_="qoo-post-item")
+            
+            post_info_list = []
+            for post in posts_list:
+                post_info = {"user": "", "link": "", "char_name": "", "guild_name": "", "desc": ""}
+            
+                post_info["user"] = post.select('a.icon.qoo-egg-view')[0]["href"]
+                post_info["link"] = 'https://user.qoo-app.com/en/card/' + post["data-id"]
+                td = post.findAll('td')
+                if len(td)>0 :
+                    post_info["char_name"] = self.normalize(td[1].get_text(strip=True))
+                if len(td)>2 :
+                    post_info["guild_name"] = self.normalize(td[3].get_text(strip=True))
+
+                desc = post.findAll('p')
+                if len(desc) > 1:
+                    post_info["desc"] = self.normalize(desc[1].get_text(strip=True))
+
+                post_info_list.append(post_info)
+            
+            with open("./cogs/blackwordlist.json", 'r', encoding='utf-8-sig') as file:
+                word_list = json.load(file)["word_list"]
+            
+            violating_post = []
+            for post in post_info_list:
+                if any(word in post["char_name"] for word in word_list) or any(word in post["desc"] for word in word_list) or any(word in post["guild_name"] for word in word_list) :
+                    violating_post.append(post)
+            
+            reply = ""
+            for post in violating_post:
+                reply = reply + post["user"] + '\n'
+                reply = reply + post["link"] + '\n'
+                
+            if reply == "":
+                reply = "No violators detected."
+            reply = reply + '\n'
+            
+            await ctx.send(reply)
+        else:
+            game_list = ["4847", "2519", "191", "11812", "7874"]
+            for i in game_list:
+                await self.card(ctx, i)
 
 def setup(bot: commands.Bot):
     bot.add_cog(detect_posts(bot))
