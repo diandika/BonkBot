42f67270196f29072a0972679455e8b90d2643ed
D Andika Putra <mohammaddicky.andika@gmail.com>
2022-03-12 20:57:00 +0700

    adjust detect function and comment

--- cogs/detect_posts.py
+++ cogs/detect_posts.py
@@ -28,25 +28,26 @@ class detect_posts(commands.Cog, name="detect posts"):
     @commands.command()
     async def detect(self, ctx):
         post_info_list = []
-        
-        while (len(post_info_list) == 0):
+        posts_list = []
+
+        while (len(posts_list) == 0):
             page_source = self.get_page_sources('https://notes.qoo-app.com/en')
             
             posts_list = page_source.findAll("div", class_="qoo-note-view")
-            
-            for post in posts_list:
-                post_info = {"user": "", "link": "", "title": "", "desc": ""}
+        
+        for post in posts_list:
+            post_info = {"user": "", "link": "", "title": "", "desc": ""}
 
-                post_info["user"] = post.findAll("a", class_="artist")[0]["href"]
-                post_info["link"] = post["data-href"]
-                title = post.findAll("strong", class_="content-title")
-                if len(title) > 0 :
-                    post_info["title"] = self.normalize(title[0].text)
-                desc = post.findAll("cite", class_="description")
-                if len(desc) > 0 :
-                    post_info["desc"] = self.normalize(desc[0].get_text(strip=True))
+            post_info["user"] = post.findAll("a", class_="artist")[0]["href"]
+            post_info["link"] = post["data-href"]
+            title = post.findAll("strong", class_="content-title")
+            if len(title) > 0 :
+                post_info["title"] = self.normalize(title[0].text)
+            desc = post.findAll("cite", class_="description")
+            if len(desc) > 0 :
+                post_info["desc"] = self.normalize(desc[0].get_text(strip=True))
 
-                post_info_list.append(post_info)
+            post_info_list.append(post_info)
            
         with open("./cogs/blackwordlist.json", 'r', encoding='utf-8-sig') as file:
             word_list = json.load(file)["word_list"]
@@ -73,28 +74,29 @@ class detect_posts(commands.Cog, name="detect posts"):
 
         if arg1 != "":
             post_info_list = []
-            
-            while (len(post_info_list)):
+            posts_list = []
+
+            while (len(posts_list) == 0):
                 page_source = self.get_page_sources('https://apps.qoo-app.com/en/app-card/' + str(arg1))
                 
                 posts_list = page_source.findAll("div", class_="qoo-post-item")
                 
-                for post in posts_list:
-                    post_info = {"user": "", "link": "", "char_name": "", "guild_name": "", "desc": ""}
-                
-                    post_info["user"] = post.select('a.icon.qoo-egg-view')[0]["href"]
-                    post_info["link"] = 'https://user.qoo-app.com/en/card/' + post["data-id"]
-                    td = post.findAll('td')
-                    if len(td)>0 :
-                        post_info["char_name"] = self.normalize(td[1].get_text(strip=True))
-                    if len(td)>2 :
-                        post_info["guild_name"] = self.normalize(td[3].get_text(strip=True))
+            for post in posts_list:
+                post_info = {"user": "", "link": "", "char_name": "", "guild_name": "", "desc": ""}
+            
+                post_info["user"] = post.select('a.icon.qoo-egg-view')[0]["href"]
+                post_info["link"] = 'https://user.qoo-app.com/en/card/' + post["data-id"]
+                td = post.findAll('td')
+                if len(td)>0 :
+                    post_info["char_name"] = self.normalize(td[1].get_text(strip=True))
+                if len(td)>2 :
+                    post_info["guild_name"] = self.normalize(td[3].get_text(strip=True))
 
-                    desc = post.findAll('p')
-                    if len(desc) > 1:
-                        post_info["desc"] = self.normalize(desc[1].get_text(strip=True))
+                desc = post.findAll('p')
+                if len(desc) > 1:
+                    post_info["desc"] = self.normalize(desc[1].get_text(strip=True))
 
-                    post_info_list.append(post_info)
+                post_info_list.append(post_info)
             
             with open("./cogs/blackwordlist.json", 'r', encoding='utf-8-sig') as file:
                 word_list = json.load(file)["word_list"]
