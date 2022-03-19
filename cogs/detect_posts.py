083d3fbb6f817c2719645f7204683859be2cafd9
D Andika Putra <mohammaddicky.andika@gmail.com>
2022-03-19 10:36:41 +0700

    revert

--- cogs/detect_posts.py
+++ cogs/detect_posts.py
@@ -7,8 +7,6 @@ import json
 import numpy as np
 from bs4 import BeautifulSoup
 from nextcord.ext import commands
-from selenium import webdriver
-import time
 
 class detect_posts(commands.Cog, name="detect posts"):
 
@@ -25,14 +23,7 @@ class detect_posts(commands.Cog, name="detect posts"):
         return new_s
 
     def get_page_sources(self, url):
-
-        url = "http://legendas.tv/busca/walking%20dead%20s03e02"
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        browser = webdriver.Chrome(chrome_options=options)
-        browser.get(url)
-        time.sleep(1)
-        page = browser.page_source
+        page = requests.get(url)
         return BeautifulSoup(page.text, "html.parser")
 
     @commands.command()
@@ -128,50 +119,50 @@ class detect_posts(commands.Cog, name="detect posts"):
             for i in game_list:
                 await self.card(ctx, i)
 
-    @commands.command()
-    async def notes(self, ctx, arg1 = ""):
-        if arg1 != "":
-            post_info_list = []
-            posts_list = []
+    # @commands.command()
+    # async def notes(self, ctx, arg1 = ""):
+    #     if arg1 != "":
+    #         post_info_list = []
+    #         posts_list = []
 
-            while (len(posts_list) == 0):
-                page_source = self.get_page_sources('https://apps.qoo-app.com/en/app-note/')
-                print(page_source)
-                posts_list = page_source.findAll("div", class_="qoo-note-view")
+    #         while (len(posts_list) == 0):
+    #             page_source = self.get_page_sources('https://apps.qoo-app.com/en/app-note/')
+    #             print(page_source)
+    #             posts_list = page_source.findAll("div", class_="qoo-note-view")
             
-            for post in posts_list:
-                post_info = {"user": "", "link": "", "title": "", "desc": ""}
+    #         for post in posts_list:
+    #             post_info = {"user": "", "link": "", "title": "", "desc": ""}
 
-                post_info["user"] = post.findAll("a", class_="artist")[0]["href"]
-                post_info["link"] = post["data-href"]
-                title = post.findAll("strong", class_="content-title")
-                if len(title) > 0 :
-                    post_info["title"] = self.normalize(title[0].text)
-                desc = post.findAll("cite", class_="description")
-                if len(desc) > 0 :
-                    post_info["desc"] = self.normalize(desc[0].get_text(strip=True))
+    #             post_info["user"] = post.findAll("a", class_="artist")[0]["href"]
+    #             post_info["link"] = post["data-href"]
+    #             title = post.findAll("strong", class_="content-title")
+    #             if len(title) > 0 :
+    #                 post_info["title"] = self.normalize(title[0].text)
+    #             desc = post.findAll("cite", class_="description")
+    #             if len(desc) > 0 :
+    #                 post_info["desc"] = self.normalize(desc[0].get_text(strip=True))
 
-                post_info_list.append(post_info)
+    #             post_info_list.append(post_info)
                 
-            with open("./cogs/blackwordlist.json", 'r', encoding='utf-8-sig') as file:
-                word_list = json.load(file)["word_list"]
+    #         with open("./cogs/blackwordlist.json", 'r', encoding='utf-8-sig') as file:
+    #             word_list = json.load(file)["word_list"]
             
-            violating_post = []
-            for post in post_info_list:
-                if any(word in post["char_name"] for word in word_list) or any(word in post["desc"] for word in word_list) or any(word in post["guild_name"] for word in word_list) :
-                    violating_post.append(post)
+    #         violating_post = []
+    #         for post in post_info_list:
+    #             if any(word in post["char_name"] for word in word_list) or any(word in post["desc"] for word in word_list) or any(word in post["guild_name"] for word in word_list) :
+    #                 violating_post.append(post)
             
-            reply = "Found " + str(len(violating_post)) + "/" + str(len(post_info_list)) + " violating posts" + "\n"
-            for post in violating_post:
-                reply = reply + post["user"] + '\n'
+    #         reply = "Found " + str(len(violating_post)) + "/" + str(len(post_info_list)) + " violating posts" + "\n"
+    #         for post in violating_post:
+    #             reply = reply + post["user"] + '\n'
                 
-            if reply != "":
-                await ctx.send(reply)
+    #         if reply != "":
+    #             await ctx.send(reply)
             
-        else:
-            game_list = ["4847", "2519", "191", "11812", "7874", "10427", "19415"]
-            for i in game_list:
-                await self.notes(ctx, i)
+    #     else:
+    #         game_list = ["4847", "2519", "191", "11812", "7874", "10427", "19415"]
+    #         for i in game_list:
+    #             await self.notes(ctx, i)
 
 def setup(bot: commands.Bot):
     bot.add_cog(detect_posts(bot))
